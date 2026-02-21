df11a688477287d67a6df0849e088e7b559a17eb
Rithikeswaran M <151950168+rithik0711@users.noreply.github.com>
2026-02-21 19:48:01 +0530

    Time: 4 ms (87.21%), Space: 42.6 MB (27.37%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -1,20 +1,21 @@
-//Used bitCount to Count number of setbits in the integer
 class Solution {
-    public int countPrimeSetBits(int left, int right) {
-        int res=0;
-        for(int i=left;i<=right;i++){
-            int count = Integer.bitCount(i);
-            if(isPrime(count)) res++;
-        }
-        return res;
-    }
     public boolean isPrime(int num){
-        if(num<2) return false;
+        if(num<=1) return false;
         if(num==2) return true;
         if(num%2==0) return false;
         for(int i=3;i*i<=num;i+=2){
             if(num%i==0) return false;
         }
         return true;
+    }
+    public int countPrimeSetBits(int left, int right) {
+        int count=0;
+        for(int i=left;i<=right;i++){
+            int ones_count = Integer.bitCount(i);
+            if(isPrime(ones_count)){
+                count++;
+            }
+        }
+        return count;
     }
 }
